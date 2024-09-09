3dea3c8fda704bc4d6a77550f5e6bd1505a0c2fa
Archana22-T <archana.gajendula13@gmail.com>
2024-09-09 06:55:59 +0530

    All Files

--- src/sept/ex_06092024/Lab006.java
+++ src/sept/ex_06092024/Lab006.java
@@ -4,8 +4,8 @@ public class Lab006 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(2+2);
-        System.out.println(2-2);
+        System.out.println(45-42);
         System.out.println(2*2);
-        System.out.println(2/2);
+        System.out.println(45/2);
     }
 }
